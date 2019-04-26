452fe1d958ba0925677bfef34fce3e9cfd1d180c
Andrew Gaudet <amg0508@gmail.com>
2019-04-26 16:26:08 -0400

    Added error checking for argv[1], argv[2], and argv[4]

--- p2.c
+++ p2.c
@@ -5,7 +5,22 @@ int main(int argc, char const *argv[])
 {
 	if (argc != 5)
 	{
-		fprintf(stderr, "ERROR: Invalid arguments\n");
+		fprintf(stderr, "ERROR: Incorrect number of arguments recieved\n");
+		return EXIT_FAILURE;
+	}
+	if (atoi(argv[1]) == 0)
+	{
+		fprintf(stderr, "ERROR: %s should be the number of frames per line\n", argv[1]);
+		return EXIT_FAILURE;
+	}
+	if (atoi(argv[2]) == 0)
+	{
+		fprintf(stderr, "ERROR: %s should be the number of frames in the memory\n", argv[2]);
+		return EXIT_FAILURE;
+	}
+	if (atoi(argv[4]) == 0)
+	{
+		fprintf(stderr, "ERROR: %s should be the time to move a frame\n", argv[4]);
 		return EXIT_FAILURE;
 	}
 
